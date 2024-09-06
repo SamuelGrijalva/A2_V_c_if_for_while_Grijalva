0fd87ffb5927f93efb59c141f3a6f0c46b1cef4b
SamuelGrijalva <a.22308051281205@cbtis128.edu.mx>
2024-09-06 09:35:16 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,21 @@
+
+nombre="Samuel Grijalva"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["Roger","Ever","Mota"]
+for chicas in MisNovias:
+    print(chicas)
+    
+    print("otro ejemplo")
+    b = "Samuel Grijalva"
+print(b[2:5])
+print(" a mayusculas")
+a = "Samuel Grijalva"
+print(a.upper())
+print("Concatenar")
+a = "Jesus"
+b = "Mota"
+c = a +"_ "+ b
+print(c)
+    
