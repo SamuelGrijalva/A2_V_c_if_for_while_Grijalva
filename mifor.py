f631585f692b537cc9c80e8ba468ee2d9c24bb50
SamuelGrijalva <a.22308051281205@cbtis128.edu.mx>
2024-09-06 10:15:30 -0600

    Add files via upload

---
+++ mifor.py
@@ -0,0 +1,4 @@
+print("Ejemplo con for, imprimir una lista de frutas")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
---
+++ miwhile.py
@@ -0,0 +1,5 @@
+print("Ejemplo con while, generar numeros del 1 al 10")
+i = 1
+while i < 6:
+  print(i)
+  i += 1
